98c24238a459b1b20dee4cee0ccf41bc24749888
bob <bob@iconica.nl>
2023-03-17 09:51:12 +0100

    feat: restore structural fundamentals of app from previous version

---
+++ lib/guestlist/guestlist.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:social_credit_club_app/model/guest.dart';
+import 'package:social_credit_club_app/scan/scanner.dart';
+
+
+class Guestlist extends StatefulWidget {
+  const Guestlist({super.key});
+
+  @override
+  State<Guestlist> createState() => _GuestlistState();
+}
+
+class _GuestlistState extends State<Guestlist> {
+
+final Future<List<Guest>> _getGuests = Future<List<Guest>>(() => [Guest(),Guest(),]);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: FutureBuilder<List<Guest>>(
+        future: _getGuests, 
+        builder: (BuildContext context, AsyncSnapshot<List<Guest>> snapshot) {
+          List<Widget> children;
+          if (snapshot.hasData) {
+            children = <Widget>[
+              Text(snapshot.data.toString(),)
+            ];
+          } else if (snapshot.hasError) {
+            children = <Widget>[
+              const Icon(
+                Icons.error_outline,
+                color: Colors.red,
+                size: 60,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: Text('Error: ${snapshot.error}'),
+              ),
+            ];
+          } else {
+            children = const <Widget>[
+              SizedBox(
+                width: 60,
+                height: 60,
+                child: CircularProgressIndicator(),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 16),
+                child: Text('Awaiting result...'),
+              ),
+            ];
+          }
+
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: children,
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const Scanner(
+                title: 'Add guest',
+              ),
+            ),
+          );
+        },
+        child: const Text('Add guest'),
+      ),
+    );
+  }
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import mobile_scanner
+import smart_auth
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
+  SmartAuthPlugin.register(with: registry.registrar(forPlugin: "SmartAuthPlugin"))
 }
